99bb488096f1c546f4145c3e373402c2e40b6ff3
mthozesk <43072994+mthozesk@users.noreply.github.com>
2019-04-14 00:50:28 -0400

    Update customers.php

--- customers.php
+++ customers.php
@@ -1,52 +1,50 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/hu.css" rel="stylesheet">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/hu_vars.js"></script>
-    <script>
-	    $.get(URL + 'session.php', function(data) { 
-            if( data == "expired" ) { 
-                location.href = "hu_login.html"; 
-            }
-        });
-		
-        
-        var id = getID();
-		console.log(id);
-        
-        function retrievePage() {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    document.getElementById("body").innerHTML = xhttp.responseText;
-                }
-            };
-            xhttp.open("GET", URL + "http://csis.svsu.edu/~mthozesk/cis355wi19/temp/customers.class.php?id=" + id, true);
-            xhttp.send();
-        }
-        
-        function selectUser() { 
-            id = document.getElementById("user_id").value;
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                    document.getElementById("body").innerHTML = xhttp.responseText;
-                }
-            };
-            xhttp.open("GET", URL + "http://csis.svsu.edu/~mthozesk/cis355wi19/temp/customers.class.php?id=" + id, true);
-            xhttp.send();
-        }
-    </script>
-</head>
+<?php
 
-<body onload="retrievePage();">
-    <div class="padding">
-        <div id="body">
-        </div>
-    </div>
-</body>
-</html>
+session_start();
+header('Access-Control-Allow-Origin: *');
+
+// include the class that handles database connections
+require "http://csis.svsu.edu/~mthozesk/Prog01_CIS355/database.php";
+//require "../../Prog01_CIS355/database.php";
+// include the class containing functions/methods for "customer" table
+// Note: this application uses "customer" table, not "cusotmers" table
+require "http://csis.svsu.edu/~mthozesk/cis355wi19/temp/customers.class.php";
+$cust = new Customer();
+ 
+// set active record field values, if any 
+// (field values not set for display_list and display_create_form)
+if(isset($_GET["id"]))          $id = $_GET["id"]; 
+if(isset($_POST["name"]))       $cust->name = htmlspecialchars($_POST["name"]);
+if(isset($_POST["email"]))      $cust->email = htmlspecialchars($_POST["email"]);
+if(isset($_POST["mobile"]))     $cust->mobile = htmlspecialchars($_POST["mobile"]);
+// we don't need htmlspecialchars() because password is never displayed in browswer!
+if(isset($_POST["password"]))     $cust->password = $_POST["password"];
+
+// "fun" is short for "function" to be invoked 
+if(isset($_GET["fun"])) $fun = $_GET["fun"];
+else $fun = "display_list"; 
+
+// switch case block for whatever the user clicks on which provokes the specific funtion
+switch ($fun) {
+    case "display_list":        $cust->list_records();
+        break;
+    case "display_create_form": $cust->create_record(); 
+        break;
+    case "display_read_form":   $cust->read_record($id); 
+        break;
+    case "display_update_form": $cust->update_record($id);
+        break;
+    case "display_delete_form": $cust->delete_record($id); 
+        break;
+    case "insert_db_record":    $cust->insert_db_record(); 
+        break;
+    case "update_db_record":    $cust->update_db_record($id);
+        break;
+    case "delete_db_record":    $cust->delete_db_record($id);
+        break;
+    default: 
+        echo "Error: Invalid function call (customer.php)";
+        exit();
+        break;
+}
+
